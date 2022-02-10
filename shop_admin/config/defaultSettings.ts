22d78e760107d98b7aa2aba02d849f56da4bf5ff
XiangYu <kisky3@126.com>
2022-02-10 23:01:06 +0900

    change color

--- shop_admin/config/defaultSettings.ts
+++ shop_admin/config/defaultSettings.ts
@@ -4,18 +4,16 @@ const Settings: LayoutSettings & {
   pwa?: boolean;
   logo?: string;
 } = {
-  navTheme: 'light',
-  // 拂晓蓝
-  primaryColor: '#1890ff',
-  layout: 'mix',
-  contentWidth: 'Fluid',
-  fixedHeader: false,
-  fixSiderbar: true,
-  colorWeak: false,
-  title: 'Ant Design Pro',
-  pwa: false,
-  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
-  iconfontUrl: '',
+  "navTheme": "light",
+  "primaryColor": "#13C2C2",
+  "layout": "side",
+  "contentWidth": "Fluid",
+  "fixedHeader": false,
+  "fixSiderbar": true,
+  "pwa": false,
+  "logo": "https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg",
+  "headerHeight": 48,
+  "splitMenus": false
 };
 
 export default Settings;
