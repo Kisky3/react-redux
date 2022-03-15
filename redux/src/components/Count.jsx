2b2253ae3496acd30be5a82d3db08da797fc17ec
XiangYu <kisky3@126.com>
2022-03-15 20:17:18 +0900

    add method

--- redux/src/components/Count.jsx
+++ redux/src/components/Count.jsx
@@ -2,19 +2,46 @@ import React, { Component } from 'react'
 import './index.css';
 
 export default class Count extends Component {
+    state = {count: 0}
+
+    increment = () => {
+        const {value} = this.selectNumber;
+        const {count} = this.state;
+        this.setState({count: count + value*1});
+    }
+    decrement = () => {
+        const {value} = this.selectNumber;
+        const {count} = this.state;
+        this.setState({count: count - value*1});
+    }
+    incrementWhenOdd = () => {
+        const {value} = this.selectNumber;
+        const {count} = this.state;
+        if(count % 2 !== 0) {
+            this.setState({count: count + value*1});
+        }
+    }
+    incrementWhenAsync = () => {
+        const {value} = this.selectNumber;
+        const {count} = this.state;
+        setTimeout(() => {
+            this.setState({count: count + value*1});
+        }, 500);
+    }
   render() {
     return (
       <div>
           <h2>Count Function</h2>
+          <h3>Count: {this.state.count}</h3>
           <select name="num" id="num" ref={ c => this.selectNumber = c} className="select">
               <option value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
           </select>
-          <button className="button"> + </button>
-          <button className="button"> - </button>
-          <button className="button"> add when odd </button>
-          <button className="button"> add when async </button>
+          <button className="button" onClick={this.increment}> + </button>
+          <button className="button" onClick={this.decrement}> - </button>
+          <button className="button" onClick={this.incrementWhenOdd}> add when odd </button>
+          <button className="button" onClick={this.incrementWhenAsync}> add when async </button>
       </div>
     )
   }
