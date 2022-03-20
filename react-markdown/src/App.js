7f3ac6fe9353641c7403144e953669561f04a922
XiangYu <kisky3@126.com>
2022-03-20 14:43:31 +0900

    first function

--- react-markdown/src/App.js
+++ react-markdown/src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import React from "react";
+import MDEditor from "@uiw/react-md-editor";
 
+const mkdStr = `# Markdown Editor for [React](https://facebook.github.io/react/)`;
 function App() {
-  return (
-    <div className="App">
-      Hello world
-    </div>
-  );
+    const [value, setValue] = React.useState(mkdStr);
+    return (
+      <div className="container">
+        <h3>Auto</h3>
+        <MDEditor height={200} value={value} onChange={setValue} />
+        <div data-color-mode="light">
+          <h3>Light</h3>
+          <MDEditor height={200} value={value} onChange={setValue} />
+        </div>
+        <h3>Auto MDEditor.Markdown</h3>
+        <MDEditor.Markdown
+          style={{ padding: 15 }}
+          source={value}
+          linkTarget="_blank"
+          previewOptions={{
+            linkTarget: "_blank"
+          }}
+        />
+      </div>
+    );
 }
 
 export default App;
