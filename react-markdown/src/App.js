83e28e30c0e90a65c8f51aa5c5c8a0d383a6fa88
XiangYu <kisky3@126.com>
2022-03-20 14:49:54 +0900

    いらないコードを削除する

--- react-markdown/src/App.js
+++ react-markdown/src/App.js
@@ -2,26 +2,11 @@ import './App.css';
 import React from "react";
 import MDEditor from "@uiw/react-md-editor";
 
-const mkdStr = `# Markdown Editor for [React](https://facebook.github.io/react/)`;
 function App() {
-    const [value, setValue] = React.useState(mkdStr);
+    const [value, setValue] = React.useState();
     return (
       <div className="container">
-        <h3>Auto</h3>
         <MDEditor height={200} value={value} onChange={setValue} />
-        <div data-color-mode="light">
-          <h3>Light</h3>
-          <MDEditor height={200} value={value} onChange={setValue} />
-        </div>
-        <h3>Auto MDEditor.Markdown</h3>
-        <MDEditor.Markdown
-          style={{ padding: 15 }}
-          source={value}
-          linkTarget="_blank"
-          previewOptions={{
-            linkTarget: "_blank"
-          }}
-        />
       </div>
     );
 }
